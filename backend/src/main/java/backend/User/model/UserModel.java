03d6a4755a4979f2ecb1dc52c1ec03444ccfd76e
malinsamethsiluni <128206984+malinsamethsiluni@users.noreply.github.com>
2025-04-28 21:40:18 +0530

    Update UserModel.java

--- backend/src/main/java/backend/User/model/UserModel.java
+++ backend/src/main/java/backend/User/model/UserModel.java
@@ -7,6 +7,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.util.HashSet;
 import java.util.Set;
 
+
 @Document(collection = "user")
 public class UserModel {
     @Id
