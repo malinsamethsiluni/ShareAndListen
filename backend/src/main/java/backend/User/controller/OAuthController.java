4957e1577c385896d43885fb4519b2bc16e994f7
malinsamethsiluni <128206984+malinsamethsiluni@users.noreply.github.com>
2025-05-05 15:54:46 +0530

    Update OAuthController.java

--- backend/src/main/java/backend/User/controller/OAuthController.java
+++ backend/src/main/java/backend/User/controller/OAuthController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.RedirectView;
 
+
 import java.util.Map;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
