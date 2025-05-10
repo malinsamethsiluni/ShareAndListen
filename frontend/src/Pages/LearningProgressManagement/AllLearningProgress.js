6deac317a16d9163d0a680b5b165cfe2918acdb1
malinsamethsiluni <128206984+malinsamethsiluni@users.noreply.github.com>
2025-05-10 18:44:31 +0530

    Update AllLearningProgress.js

--- frontend/src/Pages/LearningProgressManagement/AllLearningProgress.js
+++ frontend/src/Pages/LearningProgressManagement/AllLearningProgress.js
@@ -6,7 +6,6 @@ import { IoIosCreate } from "react-icons/io";
 import { BsFillGridFill, BsListUl } from "react-icons/bs";
 import Modal from 'react-modal';
 
-
 Modal.setAppElement('#root');
 
 function AllLearningProgress() {
