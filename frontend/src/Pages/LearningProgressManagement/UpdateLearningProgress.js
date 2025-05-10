96ac611659883f956278535d807fb24d9cdb27ce
malinsamethsiluni <128206984+malinsamethsiluni@users.noreply.github.com>
2025-05-10 21:58:04 +0530

    Update UpdateLearningProgress.js

--- frontend/src/Pages/LearningProgressManagement/UpdateLearningProgress.js
+++ frontend/src/Pages/LearningProgressManagement/UpdateLearningProgress.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import NavBar from '../../Components/NavBar/NavBar';
 
+
 function UpdateLearningProgress() {
   const { id } = useParams();
   const [formData, setFormData] = useState({
